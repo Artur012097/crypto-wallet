6248d532d53eda748442fe66feb3572ee604d651
Artur012097 <artur012097.ax@gmail.com>
2022-05-17 10:46:42 +0400

    fix timer

--- assets/js/timer.js
+++ assets/js/timer.js
@@ -1,8 +1,8 @@
-let monthsBlock
-let daysBlock
-let hoursBlock
-let minutesBlock
-let secondesBlock
+const monthsBlock = document.getElementById('months')
+const daysBlock = document.getElementById('days')
+const hoursBlock = document.getElementById('hours')
+const minutesBlock = document.getElementById('minutes')
+const secondesBlock = document.getElementById('secondes')
 
 const getEndDate = async (data) => {
     const url = 'https://dev.easydev.group/api/to_start/'
@@ -37,85 +37,12 @@ const timer = (endTime) => {
     secondesBlock.innerText = `${seconds < 10 ? `0${seconds}` : seconds}`
 }
 
-const createTimer = async () => {
-
-    const timerBlock = document.getElementById('timer')
-    const endTime = await getEndDate() * 1000 + new Date().getTime()
-    let interval
-
-    const timerContent = `
-        <div>
-            <span class="value" id="months">
-                0
-            </span>
-            <span class="mean">
-                months
-            </span>
-        </div>
-        <div>
-            <span class="value" id="days">
-                0
-            </span>
-            <span class="mean">
-                days
-            </span>
-        </div>
-        <div>
-            <span class="value hours" id="hours">
-                0
-            </span>
-            <span class="mean">
-                hours
-            </span>
-        </div>
-        <div>
-            <span class="value minutes" id="minutes">
-                0
-            </span>
-            <span class="mean">
-                minutes
-            </span>
-        </div>
-        <div>
-            <span class="value secondes" id="secondes">
-                0
-            </span>
-            <span class="mean">
-                seconds
-            </span>
-        </div>
-    `
-
-    if (window.innerWidth < 1024) {
-        timerBlock.innerHTML = timerContent
-        
-        monthsBlock = document.getElementById('months')
-        daysBlock = document.getElementById('days')
-        hoursBlock = document.getElementById('hours')
-        minutesBlock = document.getElementById('minutes')
-        secondesBlock = document.getElementById('secondes')
-
-        interval = await setInterval(() => {
-            timer(endTime)
-        }, 1000);
-
-        return
-    }
-
-    clearInterval(interval)
-    timerBlock.innerHTML = ''
-}
-
 document.addEventListener('DOMContentLoaded', async () => {
+    const endTime = await getEndDate() * 1000 + new Date().getTime()
 
-    if (window.innerWidth < 1024) {
-        await createTimer()
-        await window.addEventListener('resize', createTimer)
-    } else {
-        await window.addEventListener('resize', () => {
-            createTimer()
-        })
-    }
+    interval = await setInterval(() => {
+        timer(endTime)
+    }, 1000);
 });
 
 
